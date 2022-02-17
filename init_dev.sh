672ff478d3b7de49c2fa10a8db800af08b12241e
Iqbal Singh <singhiqbal1007@gmail.com>
2022-02-17 09:23:33 +0000

    webpacker dev

--- init_dev.sh
+++ init_dev.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 rm -f /rails_oauth_practice/tmp/pids/server.pid
 if [[ "${REMOVE_GEMFILE_LOCK}" == "true" ]]; then
   rm -f /rails_oauth_practice/Gemfile.lock
 fi
-bundle install
 bundle exec rails db:migrate RAILS_ENV=development
+bundle install
