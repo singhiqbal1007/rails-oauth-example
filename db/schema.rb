bfb3fa27143afddee2f4c621faf4531a486d2d5c
Iqbal Singh <singhiqbal1007@gmail.com>
2022-02-17 13:35:19 +0000

    change db from mysql to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2022_02_04_201046) do
 
-  create_table "active_sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_201046) do
     t.index ["user_id"], name: "index_active_sessions_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
