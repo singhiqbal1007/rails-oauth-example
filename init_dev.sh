437f57d8e1dcfe565a1849e0f65ffdd906362ff8
Iqbal Singh <singhiqbal1007@gmail.com>
2022-02-18 14:39:53 +0000

    fixed stuff

--- init_dev.sh
+++ init_dev.sh
@@ -4,6 +4,5 @@ rm -f /rails_oauth_practice/tmp/pids/server.pid
 if [[ "${REMOVE_GEMFILE_LOCK}" == "true" ]]; then
   rm -f /rails_oauth_practice/Gemfile.lock
 fi
-bundle exec rails db:migrate RAILS_ENV=development
-bundle config set --local path './vendor/bundle'
 bundle install
+bundle exec rails db:migrate RAILS_ENV=development
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,29 +5,29 @@ RSpec.describe User, :type => :model do
 
   context 'validations' do
 
-    it "should be valid" do
+    it 'should be valid' do
       expect(user).to be_valid
     end
 
-    it "should have email" do
+    it 'should have email' do
       user.email = nil
       expect(user).not_to be_valid
     end
 
-    it "email should be unique" do
+    it 'email should be unique' do
       email = user.email
       user.save!
       invalid_user = build(:user, email: email)
       expect(invalid_user).not_to be_valid
     end
 
-    it "email should be saved as lowercase" do
+    it 'email should be saved as lowercase' do
       email = "UPPERCASE_EMAIL@UPPERCASE.COM"
       valid_user = create(:user, email: email)
       expect(valid_user.email).to eq(email.downcase)
     end
 
-    it "email should be valid" do
+    it 'email should be valid' do
       invalid_emails = %w[foo foo@ foo@bar.]
 
       invalid_emails.each do |invalid_email|
@@ -36,30 +36,30 @@ RSpec.describe User, :type => :model do
       end
     end
 
-    it "cannot create user without password" do
+    it 'cannot create user without password' do
       user.password = nil
       expect(user).not_to be_valid
     end
 
-    it "should respond to confirmed?" do
+    it 'should respond to confirmed?' do
       expect(user.confirmed?).to be_falsey
       user.confirmed_at = Time.now
       expect(user.confirmed?).to be_truthy
     end
 
-    it "should respond to unconfirmed?" do
+    it 'should respond to unconfirmed?' do
       expect(user.unconfirmed?).to be_truthy
       user.confirmed_at = Time.now
       expect(user.unconfirmed?).to be_falsey
     end
 
-    it "should respond to reconfirming?" do
+    it 'should respond to reconfirming?' do
       expect(user.reconfirming?).to be_falsey
       user.confirmed_email = "random@email.com"
       expect(user.reconfirming?).to be_truthy
     end
 
-    it "should respond to unconfirmed_or_reconfirming?" do
+    it 'should respond to unconfirmed_or_reconfirming?' do
       expect(user.unconfirmed_or_reconfirming?)
     end
 
