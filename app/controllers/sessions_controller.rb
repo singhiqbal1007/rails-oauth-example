73e6fe84277e42391acc977d137cb974ed16b5a2
Iqbal Singh <singhiqbal1007@gmail.com>
2022-02-26 09:22:12 +0000

    OIDC login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -45,17 +45,72 @@ class SessionsController < ApplicationController
   def new; end
 
 
+  # === OIDC LOGIN ===
+  #### Step 1: https://openid.net/specs/openid-connect-basic-1_0.html#AuthenticationRequest
+  # Generate Authorization URL with scope=email, prompt=consent, redirect_uri=callback_url and response_type=code
+  #
+  #### Step 2: https://openid.net/specs/openid-connect-basic-1_0.html#CodeRequest
+  # Send the Authentication Request
+  #
+  #### Step 3: https://openid.net/specs/openid-connect-basic-1_0.html#Authenticates
+  # Authorization Server Authenticates End-User
+  #
+  #### Step 4: https://openid.net/specs/openid-connect-basic-1_0.html#Consent
+  # User gets consent
   def oidc_login
-    client = GoogleOidc.new(root_url)
-    binding.pry
-    GoogleOidc.authorization_url(session)
+    client = GoogleOidc.client(root_url)
+    session[:state] = SecureRandom.hex(16)
+
+    # generate the authorization_uri
+    auth_uri = client.authorization_uri(
+      scope: [:openid, :email],
+      state: session[:state],
+      prompt: 'consent',
+      response_type: 'code'
+    )
+    redirect_to auth_uri
   end
 
+
+  #### Step 5: https://openid.net/specs/openid-connect-basic-1_0.html#CodeResponse
+  # Get the code provided the Authentication Server
+  #
+  #### Step 6: https://openid.net/specs/openid-connect-basic-1_0.html#ObtainingTokens
+  # Obtain ID token
+  #
+  # Step 7: https://openid.net/specs/openid-connect-basic-1_0.html#IDToken
+  # Get User email from Id Token
+  #
+  # Step 8: Login the user with the email
   def oauth_callback
-    authorization_code = params[:code]
     client = GoogleOidc.client(root_url)
 
+    # get the authorization code from parameters
+    client.authorization_code = params[:code]
 
+    # request for token
+    token = client.access_token!
 
+    # if code is correct, token will be received
+    if token
+      hash = JSON::JWT.decode(token.id_token, :skip_verification)
+      email = hash['email']
+      if email.present?
+        user = User.find_by(email: email)
+        if user
+          login user
+          redirect_to account_path
+        else
+          flash.now[:alert] = I18n.t('oauth_login_failed')
+          render :new, status: :unprocessable_entity
+        end
+      else
+        redirect_to root_url,  alert: I18n.t('oauth_login_error')
+      end
+    else
+      # redirect user to homepage in case of incorrect code
+      redirect_to root_url
+    end
+    
   end
 end
--- app/helpers/oidc_login_helper.rb
+++ app/helpers/oidc_login_helper.rb
@@ -4,53 +4,54 @@ module OidcLoginHelper
   class GoogleOidc
     CONFIG_ENDPOINT = '.well-known/openid-configuration'
 
-    def initialize(root_url)
-      config = get_config
-      OpenIDConnect::Client.new(
-        identifier: ENV['GOOGLE_CLIENT_ID'],
-        secret: ENV['GOOGLE_CLIENT_SECRET'],
-        redirect_uri: "#{url_chomp(root_url)}/auth_callback",
-        host: config.issuer,
-        authorization_endpoint: config.authorization_endpoint,
-        token_endpoint: config.token_endpoint
-      )
-    end
-
-    def authorization_url(session)
-      session[:state] = SecureRandom.hex(16)
-      client.authorization_uri(
-        scope: [:openid, :email],
-        state: session[:state],
-        prompt: 'consent'
-      )
-    end
-
-    private
-
-    def get_config
-      # get client configs from database
-      config = OidcConfigs.find_by(name: 'google')
-      if !config.nil? && !config.authorization_endpoint.nil? && config.updated_at > 1.day.ago
-        config
-      else
-        # if database configs are older than 1 day then get it from google endpoint
-        uri = URI.parse("#{config.issuer}/#{CONFIG_ENDPOINT}")
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        response = http.get(uri.request_uri)
-        hash = JSON.parse response.body
-        config.authorization_endpoint = hash['authorization_endpoint']
-        config.token_endpoint = hash['token_endpoint']
-        config.save!
-        config
+    class << self
+      def client(root_url)
+        @client ||= new_client(root_url)
       end
-    end
 
-    def url_chomp(u)
-      uri = URI.parse(u.to_s)
-      uri.path.squeeze!('/')
-      uri.path.chomp!('/')
-      uri.to_s
+      def get_token
+
+      end
+
+      private
+
+      def new_client(root_url)
+        config = get_config
+        OpenIDConnect::Client.new(
+          identifier: ENV['GOOGLE_CLIENT_ID'],
+          secret: ENV['GOOGLE_CLIENT_SECRET'],
+          redirect_uri: "#{url_chomp(root_url)}/oauth_callback",
+          host: config.issuer,
+          authorization_endpoint: config.authorization_endpoint,
+          token_endpoint: config.token_endpoint
+        )
+      end
+
+      def get_config
+        # get client configs from database
+        config = OidcConfigs.find_by(name: 'google')
+        if !config.nil? && !config.authorization_endpoint.nil? && config.updated_at > 1.day.ago
+          config
+        else
+          # if database configs are older than 1 day then get it from google endpoint
+          uri = URI.parse("#{config.issuer}/#{CONFIG_ENDPOINT}")
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          response = http.get(uri.request_uri)
+          hash = JSON.parse response.body
+          config.authorization_endpoint = hash['authorization_endpoint']
+          config.token_endpoint = hash['token_endpoint']
+          config.save!
+          config
+        end
+      end
+
+      def url_chomp(u)
+        uri = URI.parse(u.to_s)
+        uri.path.squeeze!('/')
+        uri.path.chomp!('/')
+        uri.to_s
+      end
     end
 
   end
